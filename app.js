62b5d52a10a32cf1cb02a2dcc2d61f3f14821792
Astral Gnome <reedkeenan@gmail.com>
2020-12-19 21:51:30 -0800

    deployed application successfully

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
-const inquirer = require("inquirer");
-
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
